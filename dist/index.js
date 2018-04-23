c65f037b0d138a29b0828ff6a440183b29e27685
belodpav <belod.pav@gmail.com>
2018-04-23 16:15:39 +0300

    (fix, issue-9) Исправит Баг

--- dist/index.js
+++ dist/index.js
@@ -16,6 +16,8 @@ var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -86,7 +88,11 @@ var Component = function (_ReactComponent) {
 
             var componentMods = _extends({}, props, mods);
 
-            var className = modDeps.reduce(function (str, dep) {
+            var modsKeysSet = [].concat(_toConsumableArray(modDeps)).filter(function (dep) {
+                return componentMods.hasOwnProperty(dep);
+            });
+
+            var className = modsKeysSet.reduce(function (str, dep) {
                 var mod = {
                     key: toFirstUpperCase(dep),
                     val: toFirstUpperCase(componentMods[dep])
